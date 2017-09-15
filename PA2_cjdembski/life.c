1ad0b657da90f5595cd983a162afb96159e6cccd
Clayton Dembski <cjdembski@wpi.edu>
2017-09-15 02:01:39 -0400

    creates a generation

--- PA2_cjdembski/life.c
+++ PA2_cjdembski/life.c
@@ -4,34 +4,65 @@
  *  Created on: Sep 11, 2017
  *      Author: Clayton Dembski
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+void nextgen(int rSize, int cSize, int **genA, int **genB);
+void printLifeArray(int rSize, int cSize,int **gen);
 
 int main(){
-	char genA;
-	char genB;
-	char genC;
+	int rSize = 3;
+	int cSize = 3;
+	int i;
 
-	while(1){
-		nextGen(sizeOf(genA)/sizeOf(genA[1]), sizeOf(genA[1])/sizeOf(genA[1][1]), genA, genB);
-		genC = genA;
-		genA = genB;
-		genB = genC;
+	int** genA = (int **)malloc(rSize*sizeof(int *));
+	if (genA) for (i=0; i<rSize; i++){
+		genA[i] = (int*)malloc(cSize * sizeof(int));
+		if (!genA[i]) exit(-1);
 	}
+	int** genB = (int **)malloc(rSize*sizeof(int *));
+		if (genB) for (i=0; i<rSize; i++){
+			genB[i] = (int*)malloc(cSize * sizeof(int));
+			if (!genB[i]) exit(-1);
+	}
+	int** genC = (int **)malloc(rSize*sizeof(int *));
+		if (genC) for (i=0; i<rSize; i++){
+			genC[i] = (int *)malloc(cSize * sizeof(int));
+			if (!genC[i]) exit(-1);
+		}
 
-return 0;
+	genA[0][0]=1;
+	genA[1][0]=1;
+	genA[2][0]=1;
+	genA[0][1]=1;
+	genA[1][1]=1;
+	genA[2][1]=1;
+	genA[0][2]=1;
+	genA[1][2]=1;
+	genA[2][2]=1;
+
+	nextgen(rSize, cSize, genA, genB);
+	printLifeArray(rSize, cSize,genB);
+	free(genA);
+	free(genB);
+	free(genC);
+ 	return 0;
 }
 
-/*************************************************************/
+/////////////////////////////////////////////////////////////////
 
-void nextgen(int rSize, int cSize, char genA[][], char genB[][]){
+void nextgen(int rSize, int cSize, int ** genA, int ** genB){
 	int r;
 	int c;
-	int jIndex;
-	int kIndex;
-	char nghbrs;
+	int jIndex=0;
+	int kIndex=0;
+	int nghbrs=0;
 
 	for(r = 0; r < rSize; r++){
-		nghbrs = 0;
+
 		for(c = 0; c < cSize; c++){
+			nghbrs = 0;
 			if(r-1 < 0){
 				jIndex = r;
 			}
@@ -39,32 +70,47 @@ void nextgen(int rSize, int cSize, char genA[][], char genB[][]){
 				jIndex = r-1;
 			}
 
-			for(; jIndex < rSize || jIndex<=r+1; jIndex++){
+			for(; jIndex < rSize && jIndex<=r+1; jIndex++){
 				if(c-1 < 0){
-					kIndex = r;
+					kIndex = c;
 				}
 				else{
-					kIndex = r-1;
+					kIndex = c-1;
 				}
-				for(; kIndex < cSize || kIndex<=r+1; kIndex++){
-					if(genA[jIndex][kIndex]==0)
+				for(; kIndex < cSize && kIndex<=c+1; kIndex++){
+					if(!(jIndex==r && kIndex == c) && genA[jIndex][kIndex]==1){
 						nghbrs++;
+					}
 				}
 			}
-
+			fflush(stdout);
 			if(genA[r][c] == 1){
-				if(!(nghbrs == 2 || nghbrs ==3))
+				if(!(nghbrs == 2 || nghbrs == 3)){
 					genB[r][c] = 0;
+				}
+				else
+					genB[r][c] = 1;
 			}
-			else{
+			else if(genA[r][c] == 0){
 				if(nghbrs == 3)
 					genB[r][c] = 1;
 			}
+			printf("%d\n", nghbrs);
+			//printLifeArray(rSize, cSize, genB);
+			fflush(stdout);
 		}
 	}
 }
 
-void printLifeArray(char gen[][]){
-
+void printLifeArray(int rSize, int cSize,int **gen){
+	int i;
+	int j;
+	printf("\n");
+	for(i = 0; i<3; i++){
+		printf("\n");
+		for(j = 0; j<3; j++)
+			printf("%d", gen[i][j]);
+	}
+	printf("\n");
 
 }
