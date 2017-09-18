872d39db66a5dee27857006f9c9a49f199cf7190
Floris van Rossum <fjvanrossum@wpi.edu>
2017-09-17 21:06:32 -0400

    Modified tree.c, created a README file

---
+++ PA3_cjdembski_fjvanrossum/README.txt
@@ -0,0 +1,7 @@
+README File for PA3_cjdembski_fjvanrossum CS2303 Assignment
+
+
+
+Sources:
+1. http://www.geeksforgeeks.org/binary-tree-set-1-introduction/
+2. http://cslibrary.stanford.edu/110/BinaryTrees.html
--- PA3_cjdembski_fjvanrossum/tree.c
+++ PA3_cjdembski_fjvanrossum/tree.c
@@ -2,7 +2,30 @@
  * tree.c
  *
  *  Created on: Sep 17, 2017
- *      Author: student
+ *      Author:Floris van Rossum & Clayton Dembski
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef int (*Compare)(const char *, const char *);
+
+struct node {
+	char *word;
+	struct node *left;
+	struct node *right;
+};
+
+struct node* newNode(char word[]){
+	struct node* node = (struct node*)malloc(sizeof(struct node));
+
+	node->word[] = word[];
+	node->left = NULL;
+	node->right = NULL;
+	return node;
+}
+
+void insert(char* key, struct node ** leaf, Compare cmp){
+
+}
